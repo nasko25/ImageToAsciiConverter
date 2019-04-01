ffcfe2d92f222e9db7e89904348d484db1d96c71
nasko25 <atanas.pashov@yahoo.com>
2019-04-01 19:55:22 +0300

    removed unnecessary java library

--- ImageToAscii.java
+++ ImageToAscii.java
@@ -1,20 +1,18 @@
-import magick.ImageInfo;
-import magick.MagickImage;
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File; 
 
 public class ImageToAscii {
 	public static void main(String args[]) throws Exception{
 		if (args.length >= 1) {
 			System.out.println(args[0]);
 		}
-		ImageInfo image = new ImageInfo("test_photos/photo1.jpg");
-		MagickImage mi = new MagickImage(image);
-		image.setPing(true);
-		System.out.println("\nImage Information: ");
-		System.out.println("Units: " + mi.getUnits() + "\nDimensions: " + mi.getDimension() + "\nColor Space: " + mi.getColorspace() + "\nFile Name: " + mi.getFileName() + "\nX and Y: " + mi.getXResolution() + "; " + mi.getYResolution());		
-		Dimension imageDimensions = mi.getDimension();
-		double width = imageDimensions.getWidth();
-		double height = imageDimensions.getHeight();
+		
+		BufferedImage image = ImageIO.read(new File("test_photos/photo1.jpg"));
+		double width = image.getWidth(); 
+		double height = image.getHeight();	
 		System.out.println("\n\nWidth and Height: " + "W: " + width + " _____ "+ "H: " + height);	
+	
 	}
 }
--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ else
 	echo "File does not exists"
 fi
 
-javac -cp jmagick-6.4.0.jar ImageToAscii.java
+javac ImageToAscii.java
 printf "OUTPUT:\n"
-java -cp ./:jmagick-6.4.0.jar ImageToAscii $1
+java ImageToAscii $1
 
 # https://www.lifewire.com/pass-arguments-to-bash-script-2200571
